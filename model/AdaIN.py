6fb9e50115b71ada673648ce256d13089de2b492
Jaemin Park <129755519+kidcat2@users.noreply.github.com>
2025-01-10 12:51:28 +0900

    Delete Colortransform.py

--- model/AdaIN.py
+++ model/AdaIN.py
@@ -11,48 +11,6 @@ from tqdm    import tqdm
 import torchvision.models as models
 from PIL import Image
 
-# Color Transform
-class colorTransform3(nn.Module):
-    def __init__(self, feature_num, control_point=64, offset_param=0.04):#config=0):
-        super(colorTransform3, self).__init__()
-        self.w = nn.Parameter(torch.tensor([0.5, 0.5], dtype=torch.float32))
-        self.softmax = nn.Softmax(dim=0)
-        self.control_point = control_point
-        #self.config = config
-        self.feature_num = feature_num #config.feature_num
-        self.epsilon = 1e-8
-        self.offset_param = offset_param
-        """
-        if self.offset_param != -1:
-            if config.trainable_offset == 1:
-                self.offset_param = nn.Parameter(torch.tensor([offset_param], dtype=torch.float32))
-            else:
-                self.offset_param = offset_param
-        """
-    def forward(self, org_img, params, color_map_control):
-        N, C, H, W = org_img.shape
-        color_map_control_x = color_map_control.clone()
-        if self.offset_param != -1:
-            params = params.reshape(N, self.feature_num, self.control_point) * self.offset_param
-        else:
-            params = params.reshape(N, self.feature_num, self.control_point)
-        color_map_control_y = color_map_control_x + params
-        color_map_control_y = torch.cat((color_map_control_y, color_map_control_y[:, :, self.control_point-1:self.control_point]), dim=2) # N x F x 17
-        color_map_control_x = torch.cat((color_map_control_x, color_map_control_x[:, :, self.control_point-1:self.control_point]), dim=2) # N x F x 17
-        img_reshaped = org_img.reshape(N, self.feature_num, -1) # rimg = N x F x ?
-        img_reshaped_val = img_reshaped * (self.control_point-1) # v = i*15
-        img_reshaped_index = torch.floor(img_reshaped * (self.control_point-1)) # v2 = floor(i * 15)
-        img_reshaped_index = img_reshaped_index.type(torch.int64) # v2 = int(floor(i * 15))
-        img_reshaped_index_plus = img_reshaped_index + 1 # v2p = v2 + 1
-        img_reshaped_coeff = img_reshaped_val - img_reshaped_index #  z = v - v2
-        img_reshaped_coeff_one = 1.0 - img_reshaped_coeff # z2 = 1 - z
-        mapped_color_map_control_y = torch.gather(color_map_control_y, 2, img_reshaped_index) # 4, 256, 65  <---  N, 256, 256
-        mapped_color_map_control_y_plus = torch.gather(color_map_control_y, 2, img_reshaped_index_plus)
-        out_img_reshaped = img_reshaped_coeff_one * mapped_color_map_control_y + img_reshaped_coeff * mapped_color_map_control_y_plus
-        out_img_reshaped = out_img_reshaped.reshape(N, C, H, W)
-        return out_img_reshaped
-
-
 # StarEnhancer Mapping Network 
 class Mapping(nn.Module):
     def __init__(self, latent_dim, channel):
